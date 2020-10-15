a4a64f72c9bfa6a38f77b89b96ca8c587813e63c
mlarcher531 <mlarcher531@gmail.com>
2020-10-15 15:36:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+
+    for (let i = 0; i < array.length; i++) {
+        let theElement = array[i]
+        newArray.push(callback(theElement))
+    }
+    return newArray;
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
